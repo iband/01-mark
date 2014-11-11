7cf249511c2c964b084e466c50a55a155320c801
Vitaly Ishkulov <vit@iva-soft.com>
2014-11-11 15:46:21 +0500

    Added text to token processing

---
+++ TextProcessor/TextProcessor/Processor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace TextProcessor
+{
+	class Processor
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+	public class Tokenizer
+	{
+		static Regex _Text = new Regex(@"[^\s\r\n_`\\]+", RegexOptions.Compiled);
+		enum State
+		{
+			start,
+			text
+		}
+
+		public static string BuildTokens(string text)
+		{
+			var tokens = new List<char>();
+			State cur_state = State.start;
+			foreach (char c in text)
+			{
+				if (_Text.IsMatch(c.ToString())) {
+					if (cur_state != State.text)
+					{
+						tokens.Add('{');
+						cur_state = State.text;
+					}
+					tokens.Add(c);
+				}
+			}
+			if (cur_state == State.text)
+				tokens.Add('}');
+			return new string(tokens.ToArray());
+		}
+	}
+}
---
+++ TextProcessor/TextProcessorTests/ProcessorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TextProcessor;
+using NUnit.Framework;
+
+namespace TextProcessorTests
+{
+	[TestFixture]
+	public class Processor_should
+	{
+		[Test]
+		public void return_text_tokens_with_text_on_input()
+		{
+			var output = TextProcessor.Tokenizer.BuildTokens("abcd e");
+			Assert.AreEqual(output, "{abcde}");
+		}
+	}
+}
