4a72ee5877de6ed3c3168b87fb9639fa4787d44b
Vitaly Ishkulov <vit@iva-soft.com>
2014-11-11 18:22:14 +0500

    Added pargraph token
    Tokenizer returns paragraph tokens with two newlines separated by spaces on input.

--- TextProcessor/TextProcessor/Processor.cs
+++ TextProcessor/TextProcessor/Processor.cs
@@ -11,34 +11,55 @@ namespace TextProcessor
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.WriteLine(@"&lt;");
 		}
 	}
 	public class Tokenizer
 	{
-		static Regex _Text = new Regex(@"[^\s\r\n_`\\]+", RegexOptions.Compiled);
+		static Regex _Text = new Regex(@"[^\r\n_`\\]+", RegexOptions.Compiled);
+		static Regex _Space = new Regex(@"[\s]+", RegexOptions.Compiled);
+		static Regex _NewLine = new Regex(@"[\r?\n]+", RegexOptions.Compiled);
 		enum State
 		{
 			start,
-			text
+			text,
+			newLine
 		}
 
 		public static string BuildTokens(string text)
 		{
 			var tokens = new List<char>();
-			State cur_state = State.start;
-			foreach (char c in text)
+			var memory = new List<char>();
+			State specCharState = State.start;
+			State textState = State.start;
+			for (int i = 0; i < text.Length; i++)
 			{
+				var c = text[i];
 				if (_Text.IsMatch(c.ToString())) {
-					if (cur_state != State.text)
+					if (!(_Space.IsMatch(c.ToString()) && specCharState == State.newLine))
 					{
-						tokens.Add('{');
-						cur_state = State.text;
+						specCharState = State.start;
+						if (textState != State.text)
+						{
+							tokens.Add('{');
+							textState = State.text;
+						}
+						tokens.Add(c);
 					}
-					tokens.Add(c);
+				}
+				else if (_NewLine.IsMatch(c.ToString()))
+				{
+					if (specCharState == State.newLine)
+					{
+						tokens.Add('}');
+						textState = State.start;
+						tokens.Add('P');
+						specCharState = State.start;
+					} else
+						specCharState = State.newLine;
 				}
 			}
-			if (cur_state == State.text)
+			if (textState == State.text)
 				tokens.Add('}');
 			return new string(tokens.ToArray());
 		}
--- TextProcessor/TextProcessorTests/ProcessorTests.cs
+++ TextProcessor/TextProcessorTests/ProcessorTests.cs
@@ -10,8 +10,14 @@ namespace TextProcessorTests
 		[Test]
 		public void return_text_tokens_with_text_on_input()
 		{
-			var output = TextProcessor.Tokenizer.BuildTokens("abcd e");
-			Assert.AreEqual(output, "{abcde}");
+			var output = TextProcessor.Tokenizer.BuildTokens("abc_d e");
+			Assert.AreEqual("{abcd e}", output);
+		}
+		[Test]
+		public void return_paragraph_tokens_with_two_newlines_on_input()
+		{
+			var output = TextProcessor.Tokenizer.BuildTokens("a b \nd\n   \r\ntxt");
+			Assert.AreEqual("{a b d}P{txt}", output);
 		}
 	}
 }
