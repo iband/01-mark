f13157321124f5d5b338cb4ca7f6afcea168aa2b
Vitaly Ishkulov <vit@iva-soft.com>
2014-11-12 17:12:43 +0500

    Rewritting parser using automaton
    Added parsing plain text

--- TextProcessor/TextProcessor/Processor.cs
+++ TextProcessor/TextProcessor/Processor.cs
@@ -9,168 +9,75 @@ namespace TextProcessor
 {
 	class Processor
 	{
-		static Regex _PTag = new Regex(@"\A(\r?\n +\r?\n)", RegexOptions.Compiled);
 		static void Main(string[] args)
 		{
 			//var scope = "abc".Substring(1, 3);
-			var result = _PTag.Match(@"
- text 
-      
-text");
-			Console.WriteLine("{0}", result.Length);
+			var result = Tokenizer.Parse("a bc");
+			Console.WriteLine("{0}", result);
 		}
 	}
 	public class Tokenizer
 	{
 		static Regex _Text = new Regex(@"[^\r\n_`\\]", RegexOptions.Compiled);
-		static Regex _Space = new Regex(@"[\s]", RegexOptions.Compiled);
+		static Regex _Space = new Regex(@"\s", RegexOptions.Compiled);
 		static Regex _NewLine = new Regex(@"[\r?\n]", RegexOptions.Compiled);
 		static Regex _PTag = new Regex(@"\A(\r?\n +\r?\n)", RegexOptions.Compiled);
-		static Regex _Backticks = new Regex(@"[`]", RegexOptions.Compiled);
-		static Regex _Underscore = new Regex(@"[_]", RegexOptions.Compiled);
+		static Regex _Backticks = new Regex(@"`", RegexOptions.Compiled);
+		static Regex _Code = new Regex(@"\A`([^`]+[^\\])`", RegexOptions.Compiled);
+		static Regex _Underscore = new Regex(@"_", RegexOptions.Compiled);
 		static Regex _Escape = new Regex(@"\\[`_\\]", RegexOptions.Compiled);
 		enum State
 		{
 			start,
 			text,
-			code
+			code,
+			newline,
+			underscore,
+			escape
 		}
 
-		public static string BuildTokens(string text)
+		public static string Parse(string text)
 		{
 			text = " " + text + "  ";
 			var tokens = new List<char>();
 			var memory = new List<char>();
 			State specCharState = State.start;
 			State textState = State.start;
+			State state = State.start;
+			State prevState = State.start;
+			var output = "";
 			for (int i = 1; i < text.Length - 2; i++)
 			{
 				var c = text[i];
 				var s = c.ToString();
-				if (_Escape.IsMatch(text.Substring(i, 2)))
+				while (true)
 				{
-					if (textState != State.text && specCharState != State.code)
+					switch (state)
 					{
-						tokens.Add('{');
-						textState = State.text;
+						case State.start:
+							if (_Escape.IsMatch(s))
+								state = State.escape;
+							else if (_Text.IsMatch(s))
+								state = State.text;
+							else if (_Backticks.IsMatch(s))
+								state = State.code;
+							else if (_NewLine.IsMatch(s))
+								state = State.newline;
+							else if (_Underscore.IsMatch(s))
+								state = State.underscore;
+							continue;
+						case State.text:
+							output += s;
+							state = prevState;
+							break;
+						
+						default:
+							break;
 					}
-					if (specCharState == State.code)
-						memory.Add(text[i + 1]);
-					else
-						tokens.Add(text[i + 1]);
-					i += 1;
-				}
-				else if (specCharState == State.code && !_Backticks.IsMatch(s))
-				{
-					memory.Add(c);
-				}
-				else if (_Text.IsMatch(s))
-				{
-					specCharState = State.start;
-					if (textState != State.text)
-					{
-						tokens.Add('{');
-						textState = State.text;
-					}
-					tokens.Add(c);
-				}
-				else if (_NewLine.IsMatch(s))
-				{
-					var length = _PTag.Match(text.Substring(i)).Length;
-					if (length != 0)
-					{
-						tokens.Add('}');
-						textState = State.start;
-						tokens.Add('P');
-						i += length - 1;
-					}
-					else
-					{
-						if (textState != State.text)
-						{
-							tokens.Add('{');
-							textState = State.text;
-						}
-						tokens.Add(c);
-					}
-				}
-				else if (_Backticks.IsMatch(s))
-				{
-					if (specCharState == State.code)
-					{
-						tokens.AddRange(memory);
-						memory.Clear();
-						tokens.Add(']');
-						specCharState = State.start;
-						textState = State.start;
-					}
-					else
-					{
-						if (textState == State.text)
-							memory.Add('}');
-						specCharState = State.code;
-						memory.Add('[');
-					}
-				}
-				else if (_Underscore.IsMatch(s))
-				{
-					Regex strongOpens = new Regex(@"\s__[^_\s]", RegexOptions.Compiled);
-					Regex strongCloses = new Regex(@"[^_\s]__\s", RegexOptions.Compiled);
-					Regex emOpens = new Regex(@"\s_[^_\s]+", RegexOptions.Compiled);
-					Regex emCloses = new Regex(@"[^_\s]+_\s", RegexOptions.Compiled);
-
-					var scope = text.Substring(i - 1, 4);
-					char toAdd = '\0';
-
-					if (emOpens.IsMatch(scope))
-					{
-						toAdd = 'E';
-					}
-					else if (emCloses.IsMatch(scope))
-					{
-						toAdd = 'e';
-					}
-					else if (strongOpens.IsMatch(scope))
-					{
-						toAdd = 'S';
-						i++;
-					}
-					else if (strongCloses.IsMatch(scope))
-					{
-						toAdd = 's';
-						i++;
-					}
-
-					if (toAdd != '\0')
-					{
-						if (textState == State.text)
-							tokens.Add('}');
-						textState = State.start;
-						tokens.Add(toAdd);
-					}
-					else
-					{
-						if (textState != State.text)
-							tokens.Add('{');
-						textState = State.text;
-						tokens.Add(c);
-					}
+					break;
 				}
 			}
-			if (textState == State.text)
-			{
-				if (memory.Any())
-				{
-					if (specCharState == State.code)
-					{
-						memory.RemoveAt(0);
-						memory[0] = '`';
-					}
-					tokens.AddRange(memory);
-				}
-				tokens.Add('}');
-			}
-			return new string(tokens.ToArray());
+			return output;
 		}
 	}
 }
--- TextProcessor/TextProcessorTests/ProcessorTests.cs
+++ TextProcessor/TextProcessorTests/ProcessorTests.cs
@@ -8,64 +8,14 @@ namespace TextProcessorTests
 	public class Processor_should
 	{
 		[Test]
-		public void return_text_tokens_with_text_on_input()
+		public void return_text_on_plain_text_input()
 		{
-			CheckTokensOutput("abcd e", "{abcd e}");
-		}
-		[Test]
-		public void return_paragraph_tokens_with_two_newlines_on_input()
-		{
-			CheckTokensOutput("a b d\n   \r\ntxt", "{a b d}P{txt}");
-		}
-		[Test]
-		public void not_return_paragraph_tokens_when_one_newline_on_input()
-		{
-			CheckTokensOutput("a b \n       d", "{a b \n       d}");
-		}
-		[Test]
-		public void return_code_tokens_with_backticks_on_input()
-		{
-			CheckTokensOutput("some text `some _code` and text", "{some text }[some _code]{ and text}");
-		}
-		[Test]
-		public void return_text_tokens_with_unpaired_backticks_on_input()
-		{
-			CheckTokensOutput("some text `some code and text", "{some text `some code and text}");
-		}
-		[Test]
-		public void return_em_opening_tokens_with_space_and_underscore_on_input()
-		{
-			CheckTokensOutput("_some text _some code _and text", "E{some text }E{some code }E{and text}");
-		}
-		[Test]
-		public void return_em_closing_tokens_with_underscore_and_space_on_input()
-		{
-			CheckTokensOutput("some text some_ code and_ text", "{some text some}e{ code and}e{ text}");
-		}
-		[Test]
-		public void return_strong_opening_tokens_with_space_and_two_underscore_on_input()
-		{
-			CheckTokensOutput("__some text __some code __and text", "S{some text }S{some code }S{and text}");
-		}
-		[Test]
-		public void return_strong_closing_tokens_with_two_underscore_and_space_on_input()
-		{
-			CheckTokensOutput("some text some__ code and__ text", "{some text some}s{ code and}s{ text}");
-		}
-		[Test]
-		public void return_text_tokens_when_wrong_number_of_underscores_on_input()
-		{
-			CheckTokensOutput("___some_text some___ code__and___ text", "{___some_text some___ code__and___ text}");
-		}
-		[Test]
-		public void return_escape_char_with_backslash_on_input()
-		{
-			CheckTokensOutput(@"\_text\_` \`not a code\` `", @"{_text_}[ `not a code` ]");
+			CheckTokensOutput("ab c d e", "ab c d e");
 		}
 
 		private void CheckTokensOutput(string input, string expectedResult)
 		{
-			var output = TextProcessor.Tokenizer.BuildTokens(input);
+			var output = TextProcessor.Tokenizer.Parse(input);
 			Assert.AreEqual(expectedResult, output);
 		}
 	}
